991e9cfe6f43721de2f988801d0afe8a78911c56
e24it017-svg <e24it017@shanmugha.edu.in>
2026-02-09 11:58:15 +0530

    Add files via upload

---
+++ print the output in two lines.c
@@ -0,0 +1,16 @@
+Print output in two lines using \n
+Test Case 1
+Output:
+Hello
+World
+Test Case 2
+Output:
+Good
+Morning
+ 
+#include <stdio.h>
+int main() {
+    printf("Hello\nworld\n");
+    printf("Good\nMorning");
+    return 0;
+}
